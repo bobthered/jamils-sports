e8829cdc8e4448f2e42a76577b2eea6471ff3a99
Bob McAleavey <bobmcaleavey@gmail.com>
2025-12-10 07:42:37 -0500

    feat: update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,72 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+	import { twMerge } from 'tailwind-merge';
 	import favicon from '$lib/assets/favicon.svg';
-	import { Container, Header } from '$lib/components';
+	import { A, Button, Container, Div, Header, Nav } from '$lib/components';
 	import '../app.css';
 
+	// $props
 	let { children } = $props();
+
+	// $state
+	let isNavOpen = $state(false);
+	const navItems = $state([
+		...['News', 'Schedule', 'Standings', 'Teams'].map((label) => ({
+			class: undefined,
+			href: `/${label.toLowerCase().split(' ').join(' ')}`,
+			label,
+			variants: ['buttonDefault', 'buttonGhost']
+		})),
+		{ class: 'text-center', href: '/sign-up', label: 'Sign Up', variants: ['buttonDefault'] }
+	]);
+	let windowOuterWidth = $state(0);
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<svelte:window bind:outerWidth={windowOuterWidth} />
+
 <Header>
-	<Container variants={['full']}>Test</Container>
+	<Container class="flex items-center justify-between py-3" variants={['full']}>
+		<A href="/" variants={['buttonDefault', 'buttonGhost']}>Logo</A>
+		<Button
+			onclick={() => (isNavOpen = !isNavOpen)}
+			isVisible={windowOuterWidth < 768}
+			variants={['icon']}
+		>
+			<Div class="relative aspect-square w-6">
+				<Div
+					class={twMerge(
+						'absolute top-1/2 left-1/2 h-0.5 w-5 -translate-x-1/2 bg-current transition duration-200',
+						isNavOpen ? 'translate-y-0 rotate-45' : '-translate-y-1 rotate-0'
+					)}
+				/>
+				<Div
+					class={twMerge(
+						'absolute top-1/2 left-1/2 h-0.5 w-5 -translate-x-1/2 bg-current transition duration-200',
+						isNavOpen ? 'translate-y-0 -rotate-45' : 'translate-y-1 rotate-0'
+					)}
+				/>
+			</Div>
+		</Button>
+		<Nav class="flex space-x-1" isVisible={windowOuterWidth >= 768}>
+			{#each navItems as { href, label, variants }}
+				<A class="px-6 py-3" {href} {variants}>{label}</A>
+			{/each}
+		</Nav>
+	</Container>
+	<Nav
+		class="border-t border-gray-950/10 dark:border-gray-50/10"
+		isVisible={windowOuterWidth < 768 && isNavOpen}
+		transition={(node: Element) => slide(node, { duration: 200 })}
+	>
+		<Container class="flex flex-col space-y-1 py-6">
+			{#each navItems as { class: className, href, label, variants }}
+				<A class={className} {href} {variants}>{label}</A>
+			{/each}
+		</Container>
+	</Nav>
 </Header>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,150 @@
 <script lang="ts">
-	import { A, Container, Div, H1, Img, P, Section } from '$lib/components';
+	import { A, Card, Container, Div, H1, Img, P, Section } from '$lib/components';
+	import H2 from '$lib/components/H2/H2.svelte';
+	import { theme } from '$lib/theme';
+	import { twMerge } from 'tailwind-merge';
+
+	// $state
+	const featuredGames = $state([
+		{
+			date: 'Friday, 7:00PM',
+			headerClasses: 'from-orange-500 to-red-500',
+			team1: 'Thunder',
+			team2: 'Warriors'
+		},
+		{
+			date: 'Saturday, 6:30PM',
+			headerClasses: 'from-blue-500 to-purple-500',
+			team1: 'Hawks',
+			team2: 'Eagles'
+		},
+		{
+			date: 'Sunday, 5:00PM',
+			headerClasses: 'from-green-500 to-teal-500',
+			team1: 'Blazers',
+			team2: 'Rockets'
+		}
+	]);
+	const teams = $state(
+		[
+			{
+				imageSrc:
+					'https://images.pexels.com/photos/71103/basketball-sports-teams-players-71103.jpeg',
+				losses: 3,
+				name: 'Thunder',
+				wins: 12
+			},
+			{
+				imageSrc: 'https://images.pexels.com/photos/974502/pexels-photo-974502.jpeg',
+				losses: 4,
+				name: 'Warriors',
+				wins: 11
+			},
+			{
+				imageSrc: 'https://images.pexels.com/photos/974501/pexels-photo-974501.jpeg',
+				losses: 5,
+				name: 'Hawks',
+				wins: 10
+			},
+			{
+				imageSrc: 'https://images.pexels.com/photos/3755445/pexels-photo-3755445.jpeg',
+				losses: 6,
+				name: 'Eagles',
+				wins: 9
+			},
+			{
+				imageSrc: 'https://images.pexels.com/photos/5586480/pexels-photo-5586480.jpeg',
+				losses: 8,
+				name: 'Blazers',
+				wins: 7
+			},
+			{
+				imageSrc: 'https://images.pexels.com/photos/8979937/pexels-photo-8979937.jpeg',
+				losses: 10,
+				name: 'Rockets',
+				wins: 5
+			}
+		].map(({ imageSrc, losses, name, wins }) => {
+			const href = `/teams/${name.toLowerCase()}`;
+			return { href, imageSrc, losses, name, wins };
+		})
+	);
 </script>
 
-<Section class="relative bg-black p-0 text-gray-50">
+<Section class="relative bg-black p-0 text-gray-50 sm:p-0 md:p-0 lg:p-0 xl:p-0">
 	<Img
 		alt="Basketball Hoop in Basketball Court"
 		class="absolute h-full w-full overflow-hidden object-cover opacity-30"
 		src="https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg"
 	/>
-	<Container class="relative py-40">
+	<Container class={twMerge(theme.getComponentVariant('Section', 'lg'), 'relative')}>
 		<Div class="flex flex-col space-y-6 text-center">
 			<H1>Welcome to Jamil's League</H1>
-			<P>Where Champions Are Made</P>
-			<Div class="flex flex-col space-y-6">
-				<A href="/sign-up">Join the League</A>
-				<A href="/schedule">View Schedule</A>
+			<P class="text-xl opacity-70">Where Champions Are Made</P>
+			<Div class="flex flex-col space-y-6 sm:mx-auto sm:flex-row sm:space-y-0 sm:space-x-6">
+				<A href="/sign-up" variants={['buttonDefault']}>Join the League</A>
+				<A href="/schedule" variants={['buttonDefault', 'buttonLight']}>View Schedule</A>
 			</Div>
+		</Div>
+	</Container>
+</Section>
+
+<Section class="text-center">
+	<Container>
+		<Div class="flex flex-col space-y-12">
+			<H2>Featured Games This Week</H2>
+			<Div class="flex flex-col space-y-6 lg:grid lg:grid-cols-3 lg:gap-6 lg:space-y-0">
+				{#each featuredGames as { date, headerClasses, team1, team2 }}
+					<Card class="flex flex-col overflow-hidden p-0">
+						<Div class={twMerge('bg-linear-to-r px-6 py-3 text-left text-white', headerClasses)}>
+							{date}
+						</Div>
+						<Div class="flex items-center justify-center space-x-6 p-6">
+							<Div class="flex flex-col text-xl font-semibold">
+								<Div>{team1}</Div>
+								<Div class="text-2xl font-semibold text-primary-500">-</Div>
+							</Div>
+							<Div>VS</Div>
+							<Div class="flex flex-col text-xl font-semibold">
+								<Div>{team2}</Div>
+								<Div class="text-2xl font-semibold text-primary-500">-</Div>
+							</Div>
+						</Div>
+					</Card>
+				{/each}
+			</Div>
+		</Div>
+	</Container>
+</Section>
+
+<Section class="bg-gray-100 text-center dark:bg-gray-900">
+	<Container>
+		<Div class="flex flex-col space-y-12">
+			<H2>Our Teams</H2>
+			<Div
+				class="flex flex-col space-y-6 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 md:grid-cols-3"
+			>
+				{#each teams as { href, imageSrc, losses, name, wins }}
+					<Card class="flex flex-col overflow-hidden p-0">
+						<Img alt={name} class="h-40 w-full object-cover lg:h-56" src={imageSrc} />
+						<Div class="flex flex-col space-y-3 p-6 text-left dark:bg-gray-800">
+							<Div class="flex flex-col">
+								<Div class="text-xl font-semibold">{name}</Div>
+								<Div class="opacity-70">Record: {wins}-{losses}</Div>
+							</Div>
+							<A {href}>View Roster</A>
+						</Div>
+					</Card>
+				{/each}
+			</Div>
+		</Div>
+	</Container>
+</Section>
+
+<Section class="text-center">
+	<Container>
+		<Div class="flex flex-col space-y-12">
+			<H2>League Standings</H2>
 		</Div>
 	</Container>
 </Section>
