cbdafeb48bc4eeb3aad518c129a65c50610f1be5
Bob McAleavey <bobmcaleavey@gmail.com>
2025-12-11 07:24:14 -0500

    feat: add remoteFunctions to config

---
+++ svelte.config.ts
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-vercel';
+import { type Config } from '@sveltejs/kit';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+const config: Config = {
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
+	extensions: ['.svelte', '.svx'],
+	preprocess: [vitePreprocess(), mdsvex()],
+	kit: { adapter: adapter(), experimental: { remoteFunctions: true } }
+};
+
+export default config;
